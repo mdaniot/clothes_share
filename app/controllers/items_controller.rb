3f43f047a74195629afb6c6ffe61fa0547751258
Daniel Velazquez <lorddaniot@gmail.com>
2022-11-22 14:55:49 -0300

    WIP Controller-Show

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,17 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :destroy]
+
+  def show
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:price, :avaliability, :product_tipe, :category, :size, :color, :brand)
+  end
+
 end
