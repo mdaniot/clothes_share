1078d1be5e8d25b0965c82636c4cfc3b3d5685b7
Afonso Garcia <afonso.guerra.garcia@gmail.com>
2022-11-22 16:32:54 -0300

    Deals actions created - New/Create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :items
+  resources :items do
+    resources :deals
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
