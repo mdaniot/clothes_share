efcbb9f73d570e3932f544cb4df9bf7bc0637d05
Cheyenne Fontes <cheyennefontes@hotmail.com>
2022-11-22 14:53:28 -0300

    teste

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,16 @@
 class ItemsController < ApplicationController
+  def new
+    @items = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:price, :availability, :item_type, :category, :size, :color, :brand, :user_id.name)
+  end
 end
