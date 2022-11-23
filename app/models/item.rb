ec86885a35b9b8ebb9fe7e54dc1670fdb7f932d6
Felipe Kummer <felipe.s.kummer@gmail.com>
2022-11-23 13:51:35 -0300

    index logic feature implemented

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
+  TYPES = ['Masculinas', 'Femininas', 'Sapatos masculinos', 'Sapatos femininos']
   belongs_to :user
   has_many :deals, dependent: :destroy
   has_one_attached :photo
+  validates :item_type, inclusion: { in: TYPES }
 end
