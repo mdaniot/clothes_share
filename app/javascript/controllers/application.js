6ba6d0a786db7e582d3269795f0b670905f1a4e7
Felipe Kummer <felipe.s.kummer@gmail.com>
2022-11-25 12:50:37 -0300

    Revert "Merge pull request #60 from mdaniot/photo-logout"
    This reverts commit 1f0c36f33e69a51e16a2372b3bde94932f6b34a9, reversing
    changes made to 4d04826e088e063fa26b1b34068aa327960c2cf3.

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -6,20 +6,4 @@ const application = Application.start()
 application.debug = false
 window.Stimulus   = application
 
-
-(function(){ var widget_id = 'eHd8XPVC4T';var d=document;var w=window;function l(){
-var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true;
-s.src = '//code.jivosite.com/script/widget/'+widget_id
-; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}
-if(d.readyState=='complete')
-{l(); console.log(1)}else{
-  if(w.attachEvent){
-    w.attachEvent('onload',l);
-    console.log(2)
-  }
-else{
-  console.log(3);
-  w.addEventListener('load',l,false);}}})();
-
-
 export { application }
